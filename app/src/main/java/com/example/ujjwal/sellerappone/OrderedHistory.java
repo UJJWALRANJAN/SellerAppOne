04d01dc673c1db24d10e91cfdb936e8ec68b2df7
Ujjwal Ranjan <ujjwalranjan88@gmail.com>
2018-10-04 15:20:58 +0530

    update first

---
+++ app/src/main/java/com/example/ujjwal/sellerappone/OrderedHistory.java
@@ -0,0 +1,16 @@
+package com.example.ujjwal.sellerappone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrderedHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ordered_history);
+
+
+
+    }
+}
